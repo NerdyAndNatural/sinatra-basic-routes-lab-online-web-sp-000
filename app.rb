c2a4bfcecbbe92b750bbeb1440ff2d606af72488
Sarah Williams <srwilliams8678@gmail.com>
2019-09-19 20:28:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/name' do
   "My name is Sarah"
   end
+  
+  get '/hometown' do
+    "My hometown is Philly"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Listen"
+  end
 end
